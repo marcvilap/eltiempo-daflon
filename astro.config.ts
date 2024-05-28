17fad4ad5a9ac09cf777d666576ed9ea5e18eea1
Marc Vila <marc@mhou.es>
2024-05-28 12:01:26 +0200

    error

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-	base: '/bc/daflon/',
+	base: '/',
 	devToolbar: { enabled: false },
 	integrations: [tailwind({ applyBaseStyles: false })],
 })
