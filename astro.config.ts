cff1561f05d2ff432c1ff20310e058fe864c9bae
Marc Vila <marc@mhou.es>
2024-05-31 11:17:09 +0200

    correction

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-	base: '/',
+	base: '/bc/daflon/',
 	devToolbar: { enabled: false },
 	integrations: [tailwind({ applyBaseStyles: false })],
 })
